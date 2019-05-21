673875ff6ce6cde3c1877c7532d68908e98fe068
Vitor Gratiere Torres <26410338+vitorgt@users.noreply.github.com>
2019-05-20 23:20:08 -0300

    temperature handler

---
+++ src/server_temp.py
@@ -0,0 +1,23 @@
+import socket
+
+
+def handler(skt, manager):
+    if manager.v:
+        print("sensor temperature handler actvated")
+    temps = []
+    while True:
+        while True:
+            data = skt.conn.recv(1024)
+            if data:
+                break
+        if manager.v:
+            print("temp handler received: " + str(data, "utf-8"))
+        instance = str(data, "utf-8").split("|")
+        if instance[1] == "TEMP":
+            if instance[3] == "C":
+                pass
+            elif instance[3] == "F":
+                instance[2] = (instance[2] - 32) * 5 / 9
+            elif instance[3] == "K":
+                instance[2] = instance[2] - 273.15
+            temps.append(instance[2])
